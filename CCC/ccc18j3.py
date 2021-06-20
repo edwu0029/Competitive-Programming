a2285f1b3c83b67df7e1c9ce80e0307a869af942
edwu0029 <83135752+edwu0029@users.noreply.github.com>
2021-06-19 20:52:13 -0400

    Added ccc18

---
+++ CCC/ccc18j3.py
@@ -0,0 +1,14 @@
+#Problem: https://dmoj.ca/problem/ccc18j3
+
+city1 = 0
+city2, city3, city4, city5 = map(int, input().split(' '))
+city2+=city1
+city3+=city2
+city4+=city3
+city5+=city4
+
+print(0, abs(city1-city2), abs(city1-city3), abs(city1-city4), abs(city1-city5))
+print(abs(city2-city1), 0, abs(city2-city3), abs(city2-city4), abs(city2-city5))
+print(abs(city3-city1), abs(city3-city2), 0, abs(city3-city4), abs(city3-city5))
+print(abs(city4-city1), abs(city4-city2), abs(city4-city3), 0, abs(city4-city5))
+print(abs(city5-city1), abs(city5-city2), abs(city5-city3), abs(city5-city4), 0)
---
+++ CCC/ccc18s1.py
@@ -0,0 +1,17 @@
+#Problem: https://dmoj.ca/problem/ccc18s1
+
+N = int(input())
+F = [0 for i in range(N)]
+sz = [0 for i in range(N)]
+for i in range(N):
+    x = int(input())
+    F[i] = x
+F.sort()
+for i in range(1, N):
+    diff = (F[i]-F[i-1])/2.0
+    sz[i-1]+=diff
+    sz[i]+=diff
+ans = sz[1]
+for i in range(1, N-1):
+    ans = min(ans, sz[i])
+print(ans)
