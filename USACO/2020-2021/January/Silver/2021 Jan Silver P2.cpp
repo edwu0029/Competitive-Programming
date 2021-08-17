c8ee0b7c3a730037e892ae5fb1da54f3c122c230
edwu0029 <83135752+edwu0029@users.noreply.github.com>
2021-08-17 16:00:10 -0400

    Added USACO 2021 Jan Silver

---
+++ USACO/2020-2021/January/Silver/2021 Jan Silver P2.cpp
@@ -0,0 +1,72 @@
++//Problem: http://usaco.org/index.php?page=viewproblem2&cpid=1087
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+#define nl '\n'
+#define sp ' '
+#define mp(a, b) make_pair(a, b)
+#define EPS 1e-9
+const int inf = 0x3f3f3f3f;
+const ll infll = 0x3f3f3f3f3f3f3f3f;
+const ll MOD = 1e9+7;
+const ll MODll = 4000004479;
+const int MAXN = 1e5+5;
+int N, Q, F[MAXN], ansl[MAXN], ansr[MAXN], lasto[102];
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    cin >> N >> Q;
+    string s; cin >> s;
+    for(int i = 0;i<=N;i++) F[i] = s[i-1];
+    int pre = F[1];
+    ansl[1] = 1;
+    for(int i = 1;i<102;i++) lasto[i] = -1;
+    lasto[F[1]] = ansl[1] = 1;
+    for(int i = 2;i<=N;i++){
+        ansl[i]+=ansl[i-1];
+        int cur = F[i];
+        if(cur>pre){
+            ansl[i]++;
+            lasto[cur] = i;
+        }else if(cur==pre){
+            pre = cur;
+        }else if(cur<pre){
+            for(int j = cur+1;j<='Z';j++) lasto[j] = -1;
+            if(lasto[cur]==-1){
+                ansl[i]++;
+                lasto[cur] = i;
+            }else{
+                pre = cur;
+            }
+        }
+        pre = cur;
+    }
+    for(int i = 1;i<102;i++) lasto[i] = -1;
+    pre = F[N];
+    ansr[N] = lasto[F[N]] = 1;
+    for(int i = N-1;i>=1;i--){
+        ansr[i]+=ansr[i+1];
+        int cur = F[i];
+        if(cur>pre){
+            ansr[i]++; lasto[cur] = i;
+        }else if(cur==pre){
+            pre = cur;
+        }else if(cur<pre){
+            for(int j = cur+1;j<='Z';j++) lasto[j] = -1;
+            if(lasto[cur]==-1){
+                ansr[i]++;
+                lasto[cur] = i;
+            }else{
+                pre = cur;
+            }
+        }
+        pre = cur;
+    }
+    for(int i = 0, a, b;i<Q;i++){
+        cin >> a >> b;
+        cout << ansl[a-1]+ansr[b+1] << nl;
+    }
+    return 0;
+}
---
+++ USACO/2020-2021/January/Silver/2021 Jan Silver P3.cpp
@@ -0,0 +1,46 @@
+//Problem: http://usaco.org/index.php?page=viewproblem2&cpid=1088
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+#define nl '\n'
+#define sp ' '
+#define mp(a, b) make_pair(a, b)
+#define EPS 1e-9
+const int inf = 0x3f3f3f3f;
+const ll infll = 0x3f3f3f3f3f3f3f3f;
+const ll MOD = 1e9+7;
+const ll MODll = 4000004479;
+const int MAXN = 1005;
+int N, F[MAXN][MAXN];
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    cin >> N;
+    for(int i = 1;i<=N;i++){
+        for(int j = 1;j<=N;j++){
+            cin >> F[i][j];
+        }
+    }
+    int cnt1 = 0;
+    for(int i = 1;i<=N;i++){
+        int c1 = 0, c2 = 0;
+        for(int j = 1;j<=N;j++){
+            if(j%2==0) c1+=F[i][j];
+            else c2+=F[i][j];
+        }
+        cnt1+=max(c1, c2);
+    }
+    int cnt2 = 0;
+    for(int i = 1;i<=N;i++){
+        int c1 = 0, c2 = 0;
+        for(int j = 1;j<=N;j++){
+            if(j%2==0) c1+=F[j][i];
+            else c2+=F[j][i];
+        }
+        cnt2+=max(c1, c2);
+    }
+    cout << max(cnt1, cnt2) << nl;
+    return 0;
+}
