4a650a83bafa3d0ccdef0468ba077752ad2860e3
edwu0029 <83135752+edwu0029@users.noreply.github.com>
2021-08-21 10:52:56 -0400

    Added USACO 2020 Jan Bronze

---
+++ USACO/2019-2020/January Contest/2020 Jan Bronze P2.cpp
@@ -0,0 +1,51 @@
+//Problem: http://usaco.org/index.php?page=viewproblem2&cpid=988
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+#define nl '\n'
+#define sp ' '
+#define mp(a, b) make_pair(a, b)
+#define EPS 1e-9
+const int inf = 0x3f3f3f3f;
+const ll infll = 0x3f3f3f3f3f3f3f3f;
+const ll MOD = 1e9+7;
+const ll MODll = 4000004479;
+const int MAXN = 1005;
+int N, b[MAXN], freq[MAXN];
+int main(){
+    freopen("photo.in", "r", stdin);
+    freopen("photo.out", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    cin >> N;
+    for(int i = 1;i<=N-1;i++) cin >> b[i];
+    for(int i = 1;i<=N;i++){
+        memset(freq, 0, sizeof(freq));
+        bool check = true;
+        int cur = i;
+        freq[cur]++;
+        vector<int>v = {i};
+        for(int j = 1;j<=N-1;j++){
+            int nxt = b[j]-cur;
+            if(1<=nxt&&nxt<=N&&freq[nxt]==0){
+                freq[nxt]++;
+                cur = nxt;
+                v.push_back(cur);
+            }else{
+                check = false;
+                break;
+            }
+        }
+        if(check){
+            for(int j = 0;j<N;j++){
+                cout << v[j];
+                if(j==N-1) cout << nl;
+                else cout << sp;
+            }
+            break;
+        }
+    }
+    return 0;
+}
---
+++ USACO/2019-2020/January Contest/2020 Jan Bronze P3.cpp
@@ -0,0 +1,47 @@
+//Problem: http://usaco.org/index.php?page=viewproblem2&cpid=989
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+#define nl '\n'
+#define sp ' '
+#define mp(a, b) make_pair(a, b)
+#define pb push_back
+#define EPS 1e-9
+const int inf = 0x3f3f3f3f;
+const ll infll = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9+7;
+int K, N;
+int solve(int X){
+    int speed = 0, dis = 0, t = 0;
+    for(;dis<K;t++){
+        if(speed<X){
+            speed++;
+        }else if(speed==X){
+            if(K-dis>X+1) speed++;
+        }else{
+            int desc = (speed-1+X)*(speed-X)/2-X;
+            int rem = K-dis-desc;
+            if(rem>2*speed+1){
+                speed++;
+            }else if(rem<=speed){
+                speed--;
+            }
+        }
+        dis+=speed;
+    }
+    return t;
+}
+int main(){
+    freopen("race.in", "r", stdin);
+    freopen("race.out", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    cin >> K >> N;
+    for(int i = 0, X;i<N;i++){
+        cin >> X;
+        cout << solve(X) << nl;
+    }
+    return 0;
+}
